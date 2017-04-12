c1356e5543f7295e9908d2e4936d80bfe9be2886
pugwoo <pugwoo@gmail.com>
2017-04-12 20:55:37 +0800

    we-vue demo

---
+++ src/html/we-vue-example.html.vue
@@ -0,0 +1,21 @@
+<template>
+  <wv-button type="primary" @click.native="myclick">{{btn}}</wv-button>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        btn: '我是按钮'
+      }
+    },
+    methods: {
+      myclick: function () {
+        this.$toast("操作成功")
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
--- src/layout/entry.js
+++ src/layout/entry.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+
+// weui vue封装
+import 'weui/dist/style/weui.css'
+import WeVue from 'we-vue'
+
 import App from '__VUE_PATH__'
 
 Vue.config.productionTip = false;
@@ -7,6 +12,9 @@ Vue.config.productionTip = false;
 // 公共的组件可以在这里引入，在子组件中可以照常使用
 Vue.use(VueResource);
 Vue.http.options.emulateJSON = true;
+
+// weui vue封装
+Vue.use(WeVue)
 
 new Vue({
     el: '#app',
