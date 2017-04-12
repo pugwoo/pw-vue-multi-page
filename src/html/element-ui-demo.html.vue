7f2962a0ad0253e66f108d5de64b220eabb76613
pugwoo <pugwoo@gmail.com>
2017-04-12 22:33:38 +0800

    element demo

---
+++ src/html/element-ui-demo.html.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="text">文字按钮</el-button>
+  </div>
+
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+
+</style>
--- src/layout/entry.js
+++ src/layout/entry.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+
 import App from '__VUE_PATH__'
 
 Vue.config.productionTip = false;
@@ -7,6 +11,8 @@ Vue.config.productionTip = false;
 // 公共的组件可以在这里引入，在子组件中可以照常使用
 Vue.use(VueResource);
 Vue.http.options.emulateJSON = true;
+
+Vue.use(ElementUI);
 
 new Vue({
     el: '#app',
