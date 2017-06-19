ef2a83e269d9254a87c7d9deef1f36d3df430205
pugwoo <pugwoo@gmail.com>
2017-06-19 15:18:09 +0800

    custom element-ui vue

---
+++ src/html/elex-demo.html.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <elex-button>测试</elex-button>
+  </div>
+
+</template>
+
+<script>
+  // elex/button.vue是复制自element-ui中的，这里的目的是演示从vue中复制组件源码出来，方便自己修改
+  import ElexButton from '../components/elex/button'
+
+  export default {
+    components: {
+      ElexButton
+    }
+  }
+</script>
+
+<style>
+
+</style>
